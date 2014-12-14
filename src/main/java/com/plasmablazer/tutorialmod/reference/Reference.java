5148a74917d846d64fe9cba8d48c85cb9ad81898
PlasmaBlazer <plasmablazerutube@gmail.com>
2014-12-14 16:28:50 +0000

    Tutorial 2

---
+++ src/main/java/com/plasmablazer/tutorialmod/reference/Reference.java
@@ -0,0 +1,8 @@
+package com.plasmablazer.tutorialmod.reference;
+
+public class Reference
+{
+    public static final String MOD_NAME = "Tutorial Mod";
+    public static final String MOD_ID = "TutorialMod";
+    public static final String MOD_VERSION = "1.0";
+}
